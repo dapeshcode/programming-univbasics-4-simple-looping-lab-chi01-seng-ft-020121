eaca0ad39adf143fe2a6bff7bf59487000b3c651
Alexandra Peccia <ampeccia@gmail.com>
2021-01-03 20:34:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do 
+    puts string
+  end
+end 
+
+def loop_message_n_times(string, num)
+  num.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  count = 0 
+  until count == array.length 
+    puts array[count]
+    count += 1 
+  end 
+end 
+
+def return_string_array(array)
+  count = 0 
+  while array.length > 0 
+    array[count]
+    count += 1 
+  end 
+end 
+  
